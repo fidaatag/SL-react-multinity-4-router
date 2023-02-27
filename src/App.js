0177fbd5540048c6015c9825cf471bfb04e993e6
fidaatag <fidaa2fidaa@gmail.com>
2023-02-27 21:37:51 +0700

    cra-styling app.css & index.css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
- import './App.css';
-import Welcome, {
-		Welcome2, 
-		Welcome3, 
-		Welcome4, 
-		Welcome5
-	} from './Welcome';
+import './App.css';
+import Button from './Button';
+
 
 function App() {
   return (
     <div className="App">
-    	<Welcome />
-    	<Welcome2 />
-    	<Welcome3 />
-		<Welcome4 text="Welcome Pakai Text" /> 
-		<Welcome5 children="Welcome Pakai Children" />
-		<Welcome5>Welcome Langsung</Welcome5>
+		<Button>Click Saya</Button>
     </div>
   );
 }
